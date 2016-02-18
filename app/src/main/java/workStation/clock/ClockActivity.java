29887340f19b3fa2b59d0b151f2983aef66f9bad
Salvatore Bellomo <bellomosalvo@gmail.com>
2016-02-18 21:48:58 +0100

    fix little bugs

--- app/src/main/java/workStation/clock/ClockActivity.java
+++ app/src/main/java/workStation/clock/ClockActivity.java
@@ -4,15 +4,11 @@ import android.app.Activity; // Required to create an activity.
 import android.os.Bundle; // A mapping from String values to various Parcelable types.
 import android.widget.AnalogClock;  // Required to create an analog clock
 import android.widget.DigitalClock;  // Required to create an digital clock
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+
 
 
 import saracraba.darkskyforecast.R;
-
+// Todo: Classes clocks are deprecated
 public class ClockActivity extends Activity {
 
     @Override
